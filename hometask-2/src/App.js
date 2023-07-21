1b4c67427b17eee9decba207a8e8b16650eb4d3b
omoev <109823323+omoev@users.noreply.github.com>
2023-07-21 09:28:08 +0600

    commit 1

---
+++ hometask-2/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const Square = ({ color, onClick }) => {
+  return (
+    <div
+      style={{
+        width: '100px',
+        height: '100px',
+        backgroundColor: color,
+        margin: '10px',
+        display: 'inline-block',
+        cursor: 'pointer',
+      }}
+      onClick={onClick}
+    />
+  );
+};
+
+const App = () => {
+  const [square1Color, setSquare1Color] = useState('red');
+  const [square2Color, setSquare2Color] = useState('blue');
+
+  const handleSquare1Click = () => {
+    setSquare1Color(square2Color);
+  };
+
+  const handleSquare2Click = () => {
+    setSquare2Color(square1Color);
+  };
+
+  return (
+    <div>
+      <Square color={square1Color} onClick={handleSquare1Click} />
+      <Square color={square2Color} onClick={handleSquare2Click} />
+    </div>
+  );
+};
+
+export default App;
